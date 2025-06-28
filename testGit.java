88b270155c218b828373669fa5db96a09d0b332c
BestYuan <2027843847@qq.com>
2025-06-28 10:59:21 +0800

    新建分支

--- testGit.java
+++ testGit.java
@@ -5,5 +5,6 @@ public class testGit {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
+        System.out.println("我这行代码是在feature分支中写的，需要添加到我远程仓库的feature分支");
     }
 }
